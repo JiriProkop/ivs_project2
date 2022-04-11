6064a000f8a6d323e75828926b2fa36bdccd2c66
PCerD01 <cerbak01.dell@gmail.com>
2022-04-11 23:10:41 +0200

    added compare_double function and some sample tests

--- src/calc_tests.cpp
+++ src/calc_tests.cpp
@@ -1,16 +1,26 @@
 #include <gtest/gtest.h>
 #include "calc_funcs.h"
 
+/**
+ * A function for comparing two double values with the accuracy of 0.000001.
+ *
+ * @param a First value.
+ * @param b Second value.
+ * @return Returns true if the numbers are equal.
+ */
+bool compare_double(double a, double b) {
+    return fabs(a - b) < 0.000001;
+}
+
 TEST(plus, normal_values)
 {
     EXPECT_EQ(add(10, 22), 32);
 }
 
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST(minus, neco) {
+    EXPECT_TRUE(compare_double(sub(4.6, 0.2), 4.4));
 }
 
-// vice na: 
-// https://stackoverflow.com/questions/62910867/how-to-run-tests-and-debug-google-test-project-in-vs-code
+TEST(odmocnina, randomtest) {
+    EXPECT_EQ(nth_root(4, 2), 2);
+}
