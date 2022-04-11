2cc511f22ce3b26ec03f16cbc2c3e95ab9bd71da
Patrik Cerbak <patrik.cerbak@seznam.cz>
2022-04-11 22:46:32 +0200

    Added nth_root and subtraction functions

--- src/calc_funcs.h
+++ src/calc_funcs.h
@@ -1,9 +1,41 @@
+#include <cmath>
+
 #ifndef _CALC_FUNCS_H
 #define _CALC_FUNCS_H
 
 int add(int a, int b)
 {
     return a + b;
+}
+
+/**
+ * A function for calculating the subtraction of two numbers.
+ * a - b
+ *
+ * @param a First number.
+ * @param b Second number.
+ * @return Returns the value of expression a - b.
+ */
+double sub(double a, double b) {
+    return a - b;
+}
+
+/**
+ * A function for calculating the nth root of a number.
+ * x ^ (1/n)
+ *
+ * @param x The base number.
+ * @param n The exponent.
+ * @return Returns the nth root of x.
+ */
+double nth_root(double x, long n)
+{
+    if(n % 2 == 0 && x < 0)
+    {
+        throw std::invalid_argument("The number cannot be negative for this n.");
+    }
+
+    return std::pow(x, 1.0/n);
 }
 
 #endif
