4a31738e9511b8c0a8e720a1848a97319a4c3f9d
Stepan Czajkowski <stepan.m.czajkowski@gmail.com>
2022-04-14 23:07:28 +0200

    functions plus, div, nth_power

--- src/calc_funcs.h
+++ src/calc_funcs.h
@@ -38,4 +38,40 @@ double nth_root(double x, long n)
     return std::pow(x, 1.0/n);
 }
 
+/**
+ * A function for adding two numbers.
+ * a + b
+ *
+ * @param a First number.
+ * @param b Second number.
+ * @return Returns the value of expression a + b.
+ */
+double plus(double a, double b){
+    return a + b;
+}
+
+/**
+ * A function for division of two numbers.
+ * a / b
+ *
+ * @param a dividend.
+ * @param b divisor.
+ * @return Returns the value of expression a / b.
+ */
+double div(double a, double b){
+    return a / b;
+}
+
+/**
+ * A function for calculating the nth_power of number .
+ * a^b
+ *
+ * @param a base number.
+ * @param b exponent.
+ * @return Returns the value of expression a^b.
+ */
+double nth_power(double a, double b){
+    return pow(a, b);
+}
+
 #endif
