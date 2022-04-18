0f316b04efe7b3ba7bd940810390a77495586d50
PCerD01 <cerbak01.dell@gmail.com>
2022-04-18 22:03:53 +0200

    added first version of profiling.cpp

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
+#include "calc_funcs.h"
 
 int main() {
-	cout << "Sample profiling source code" << endl;
+	std::string line;
+    double number;
+    double sum = 0;
+    double sum_squared = 0;
+    double num_of_lines = 0;
+
+    std::getline(std::cin, line);
+    while(line != "") {
+        num_of_lines += 1;
+        number = std::stod(line);
+
+        sum = plus(sum, number);
+        sum_squared = plus(sum_squared, nth_power(number, 2));
+
+        std::getline(std::cin, line);
+    }
+
+    double average = divide(sum, num_of_lines);
+    double bracket = sub(sum_squared, mul(num_of_lines, nth_power(average, 2)));
+
+    double fraction = divide(1, sub(num_of_lines, 1));
+
+    double result = nth_root(mul(fraction, bracket), 2);
+
+    printf("%lf\n", result);
+
 	return 0;
 }
 
