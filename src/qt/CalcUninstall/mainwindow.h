22314af3187d9fd8d278d8e7152288ca4e0b9e63
PCerD01 <cerbak01.dell@gmail.com>
2022-04-27 20:56:43 +0200

    added uninstaller

---
+++ src/qt/CalcUninstall/mainwindow.h
@@ -0,0 +1,26 @@
+/**
+ * @file mainwindow.cpp
+ * @author Patrik Čerbák (xcerba00)
+ * @brief  calculator uninstaller
+ */
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
