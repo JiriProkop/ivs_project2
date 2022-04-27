0d55d843fdbb0366cc0437b3293fbac071f8abd7
PCerD01 <cerbak01.dell@gmail.com>
2022-04-27 15:27:53 +0200

    refactored profiling.cpp and edited it to also take numbers splited by space and tab

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -8,33 +8,57 @@
 #include <string>
 #include "calc_funcs.h"
 
-int main() {
-	std::string line;
-    double number;
-    double sum = 0;
-    double sum_squared = 0;
-    double num_of_lines = 0;
+int main()
+{
+	char number_str[30] = {0}; // string to save the number from stdin
+    int character = getchar(); // character variable
 
-    std::getline(std::cin, line);
-    while(line != "") {
-        num_of_lines += 1;
-        number = std::stod(line);
+    double number; // number (converted from number_str)
+    double sum = 0; // sum of the numbers
+    double sum_squared = 0; // sum of the numbers squared
+    double num_of_numbers = 0; // number of values (numbers)
 
+    // go through all the characters until EOF
+    while(character != EOF)
+    {
+        // save each number into number_str
+        int i = 0;
+        while(character != ' ' && character != '\n' && character != '\t' && character != EOF)
+        {
+            number_str[i++] = character;
+            character = getchar();
+        }
+        number_str[i] = '\0';
+
+        num_of_numbers += 1;
+        number = std::stod(number_str); // convert
+
+        // calculate sums
         sum = plus(sum, number);
         sum_squared = plus(sum_squared, nth_power(number, 2));
 
-        std::getline(std::cin, line);
+        if(character == EOF)
+        {
+            break;
+        }
+        else
+        {
+            character = getchar();
+        }
     }
 
-    double average = divide(sum, num_of_lines);
+    // calculate the standard deviation operation by operation
+    double average = divide(sum, num_of_numbers);
+
     double bracket = nth_power(average, 2);
-    bracket = mul(num_of_lines, bracket);
+    bracket = mul(num_of_numbers, bracket);
     bracket = sub(sum_squared, bracket);
 
-    double fraction = sub(num_of_lines, 1);
+    double fraction = sub(num_of_numbers, 1);
     fraction =  divide(1, fraction);
 
     double result = mul(fraction, bracket);
+    result = nth_root(result, 2);
 
     printf("%lf\n", result);
 
